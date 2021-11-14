0ef7d3b6ed1a24b75b02c4d23fcffe2fe6ed4620
dimait <dima@surfly.com>
2021-11-14 22:46:27 +0100

    initial
    Pages
    - index, with demo issue present
    - log/debugger
    - breakpoints

---
+++ static/js/log-debugger.js
@@ -0,0 +1,18 @@
+const termsCheckbox = document.getElementById('terms');
+let termsAccepted = false;
+
+termsCheckbox.onchange = () => {
+  termsCheckbox.removeAttribute('aria-invalid');
+  termsAccepted = termsCheckbox.value;
+};
+
+function next() {
+  if (termsAccepted !== true) {
+    console.log('termsAccepted:', termsAccepted);
+    termsCheckbox.setAttribute('aria-invalid', 'true');
+    alert('Accept terms first!');
+    return;
+  }
+
+  location.href = 'log-debugger.html';
+}
